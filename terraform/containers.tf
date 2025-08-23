f51d482a128b0ca3740b58232a5c87282e4b6018
Marcin Bator <marcinbator.ofc@gmail.com>
2025-08-23 11:44:43 +0200

    tf cleanup

--- terraform/containers.tf
+++ terraform/containers.tf
@@ -1,11 +1,11 @@
 resource "azurerm_log_analytics_workspace" "log_analytics" {
-  name                = "example-log-analytics"
+  name                = var.container_log_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_container_app_environment" "container_env" {
-  name                       = "example-container-env"
+  name                       = var.container_env_name
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,37 +2,38 @@ variable "resource_group_name" {
   type        = string
   default = "pytest_resources"
 }
-
+variable "container_log_name" {
+  type        = string
+  default     = "pytest-log-analytics"
+}
+variable "container_env_name" {
+  type        = string
+  default     = "pytest-container-env"
+}
 variable "acr_name" {
   type        = string
   default = "pytestregistry"
 }
-
 variable "app_name" {
   type        = string
   default     = "pytest-app"
 }
-
 variable "backend_port" {
   type        = string
   default     = "8080"
 }
-
 variable "frontend_port" {
   type        = string
   default     = "3000"
 }
-
 variable "text_content" {
   type        = string
   default     = "default!"
 }
-
 variable "frontend_url" {
   type        = string
   default     = "http://localhost"
 }
-
 variable "backend_url" {
   type        = string
   default     = "http://localhost:8080"
